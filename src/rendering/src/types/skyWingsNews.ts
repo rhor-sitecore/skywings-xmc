460c36132a6520eb567717b00e6699100126f409
jzi <jzi@sitecore.net>
2025-06-18 19:02:59 -0400

    Reverted changes to existing components because storybook issues Duplicated component instead. Updated content

--- src/rendering/src/types/skyWingsNews.ts
+++ src/rendering/src/types/skyWingsNews.ts
@@ -1,12 +1,15 @@
 import { Field, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
 
-export type News = {
+export type SkyWingsNews = {
   name: Field<string>;
   fields: {
     Title: Field<string>;
     Excerpt: Field<string>;
     PublishDate: Field<string>;
     Image: ImageField;
+    Author: Field<string>;
+    ReadTime: Field<string>;
+    Category: Field<string>;
   };
   url: string;
 };
