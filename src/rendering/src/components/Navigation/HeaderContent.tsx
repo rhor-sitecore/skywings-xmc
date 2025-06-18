605ea5ba9ab2664bedc0a61f3dfb1f79c96db510
jzi <jzi@sitecore.net>
2025-06-18 19:08:11 -0400

    Removed language switcher

--- src/rendering/src/components/Navigation/HeaderContent.tsx
+++ src/rendering/src/components/Navigation/HeaderContent.tsx
@@ -75,12 +75,6 @@ const HeaderContent = (props: HeaderContentProps): JSX.Element => {
 
   return (
     <>
-      <div className={`header-eyebrow ${sxaStyles}`}>
-        <div className="content">
-          {languageSelector}
-          {links}
-        </div>
-      </div>
       <Placeholder name="jss-header-content" rendering={props.rendering} />
     </>
   );
